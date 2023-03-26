4c7f9e3a0c4f359c7b438306eab4a8253c13e1bf
anuragdeFUAS <anurag.de@stud.fra-uas.de>
2023-03-26 09:14:17 +0200

    minor changes to csvfilereader class

--- mySEProject/AnomalyDetectionSample/CSVFileReader.cs
+++ mySEProject/AnomalyDetectionSample/CSVFileReader.cs
@@ -36,8 +36,14 @@ namespace AnomalyDetectionSample
                 // Loop through each column in the current line
                 for (int j = 0; j < columns.Length; j++)
                 {
-                    // Parsing the current column as double,then adding it to the current sequence
-                    sequence.Add(double.Parse(columns[j]));
+                    if (double.TryParse(columns[j], out double value))
+                    {
+                        sequence.Add(value);
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Non-numeric value found! Please check the file.");
+                    }
                 }
                 sequences.Add(sequence);
             }
